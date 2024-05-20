9ef9f4bbbb9b08a65a408d0a28f443695b64f8e9
nikita-t1 <26936278+nikita-t1@users.noreply.github.com>
2024-05-20 07:13:36 +0200

    feat: customize calendar settings in home page
    Added ability to configure starting day of week and visibility of week numbers on the home page calendar. The configuration is provided by the new 'calendarSettingsProvider'. Also added a utility function to convert weekdays to 'StartingDayOfWeek' enum.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:kalendae/providers/calendar_settings_provider.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 import '../providers/visible_calendar_month_provider.dart';
@@ -10,18 +11,19 @@ class HomePage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     var calMonth = ref.watch(visibleCalendarMonthProvider);
+    var calendarSettings = ref.watch(calendarSettingsProvider);
 
     return Scaffold(
       body: TableCalendar(
         headerVisible: false,
-        startingDayOfWeek: StartingDayOfWeek.monday,
+        startingDayOfWeek: weekdayToStartingDayOfWeek(calendarSettings.firstDayOfWeek),
         sixWeekMonthsEnforced: true,
         onPageChanged: (focusedDay) => calMonth.selectMonth(focusedDay),
         firstDay: DateTime.utc(1970),
         lastDay: DateTime.utc(2038, 1, 18),
         focusedDay: calMonth.visibleMonth,
         // onDaySelected: (selectedDay, focusedDay) => calMonth.selectMonth(selectedDay),
-        weekNumbersVisible: true,
+        weekNumbersVisible: calendarSettings.showCalendarWeeks,
         calendarStyle: CalendarStyle(
           isTodayHighlighted: true,
           selectedDecoration: BoxDecoration(
@@ -35,5 +37,26 @@ class HomePage extends ConsumerWidget {
         ),
       ),
     );
+  }
+}
+
+StartingDayOfWeek weekdayToStartingDayOfWeek(int weekday) {
+  switch (weekday) {
+    case DateTime.monday:
+      return StartingDayOfWeek.monday;
+    case DateTime.tuesday:
+      return StartingDayOfWeek.tuesday;
+    case DateTime.wednesday:
+      return StartingDayOfWeek.wednesday;
+    case DateTime.thursday:
+      return StartingDayOfWeek.thursday;
+    case DateTime.friday:
+      return StartingDayOfWeek.friday;
+    case DateTime.saturday:
+      return StartingDayOfWeek.saturday;
+    case DateTime.sunday:
+      return StartingDayOfWeek.sunday;
+    default:
+      return StartingDayOfWeek.sunday;
   }
 }
