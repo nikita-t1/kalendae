2108746e12487479f58814a7dd156c3a2c9b423c
nikita-t1 <26936278+nikita-t1@users.noreply.github.com>
2024-05-31 20:29:45 +0200

    change: calendar event date and time formatting
    The formatting of dates and times for calendar events have been updated. Import of the intl package was necessary for this modification. A redundant comment block was also removed to clean up the code. Different ways were introduced to format the event time based on different conditions like if it's an all-day event or if the start and end time of the event are the same.

--- lib/pages/home_sections/calendar_events_section.dart
+++ lib/pages/home_sections/calendar_events_section.dart
@@ -1,5 +1,7 @@
+import 'package:device_calendar/src/models/event.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/intl.dart';
 import 'package:kalendae/extensions/CalendarExtension.dart';
 import 'package:kalendae/providers/calendar_events_provider.dart';
 import 'package:kalendae/providers/calendar_widget_state_provider.dart';
@@ -38,14 +40,13 @@ class CalendarEventsSection extends ConsumerWidget {
                     Text(
                       calendarEvents[index].title ?? 'No title',
                       style: const TextStyle(
-                        fontSize: 18.0,
+                        fontSize: 16.0,
                         fontWeight: FontWeight.bold,
                       ),
                     ),
-                    Text(
-                      calendarEvents[index].start?.toIso8601String() ?? 'No start',
+                    Text(_formatEventTime(calendarEvents[index]),
                       style: const TextStyle(
-                        fontSize: 16.0,
+                        // fontSize: 16.0,
                       ),
                     ),
                   ],
@@ -70,59 +71,25 @@ class CalendarEventsSection extends ConsumerWidget {
         }
       ),
     );
-    // return Scaffold(
-    //   body: calendarEventsAsyncValue.when(
-    //     data: (calendarEvents) {
-    //       return calendarEvents.isNotEmpty
-    //           ? ListView.builder(
-    //         itemCount: calendarEvents.length,
-    //         itemBuilder: (BuildContext context, int index) {
-    //           return Container(
-    //             margin: const EdgeInsets.all(8.0),
-    //             padding: const EdgeInsets.all(8.0),
-    //             decoration: BoxDecoration(
-    //               //get color from calendar (calendarEvents[index].calendar.color)
-    //               color: Color(calendarEvents[index].calendar?.color ?? 255),
-    //               borderRadius: BorderRadius.circular(8.0),
-    //             ),
-    //             child: Column(
-    //               crossAxisAlignment: CrossAxisAlignment.start,
-    //               children: [
-    //                 Text(
-    //                   calendarEvents[index].title ?? 'No title',
-    //                   style: const TextStyle(
-    //                     fontSize: 18.0,
-    //                     fontWeight: FontWeight.bold,
-    //                   ),
-    //                 ),
-    //                 Text(
-    //                   calendarEvents[index].start?.toIso8601String() ?? 'No start',
-    //                   style: const TextStyle(
-    //                     fontSize: 16.0,
-    //                   ),
-    //                 ),
-    //               ],
-    //             ),
-    //           );
-    //         },
-    //       )
-    //           : const Center(child: Text('No events found'));
-    //     },
-    //     loading: () => const Center(child: CircularProgressIndicator()),
-    //     error: (error, stackTrace) {
-    //       debugPrintStack(stackTrace: stackTrace);
-    //       print('Error loading events: $error');
-    //       return Center(
-    //         child: Column(
-    //           children: [
-    //             Text('Error loading events'),
-    //             Text('Please check your permissions'),
-    //           ],
-    //         ),
-    //       );
-    //     }
-    //   ),
-    // );
+  }
+
+  String _formatEventTime(Event calendarEvent) {
+    bool allDay = calendarEvent.allDay ?? false;
+    if (allDay) {
+      return _formatDate(calendarEvent.start!);
+    }
+    if (calendarEvent.start == calendarEvent.end) {
+      return _formatTime(calendarEvent.start!);
+    }
+    return '${_formatTime(calendarEvent.start!)} - ${_formatTime(calendarEvent.end!)}';
+  }
+
+  String _formatTime(DateTime dateTime) {
+    return DateFormat('HH:MM').format(dateTime);
+  }
+
+  String _formatDate(DateTime dateTime) {
+    return DateFormat('dd MMMM').format(dateTime);
   }
 }
 
